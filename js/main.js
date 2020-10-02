a65578a26a1140f81cdd0f2fc139ab80d3ebf1ff
komradsql <65289841+komradsql@users.noreply.github.com>
2020-10-02 12:14:23 +0300

    Update everything
    Added new pictures, fix the bg-width

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 var imgHead = [
     './images/1.jpg',
-    './images/2.jpg',
-    './images/3.jpg',
-    './images/4.jpg',
-    './images/5.jpg',
+    './images/10.jpg',
+    './images/15.jpg',
+    './images/11.jpg',
+    './images/14.jpg',
     './images/6.jpg',
     './images/7.jpg',
-    './images/8.jpg'
+    './images/8.jpg',
+    './images/9.jpg',
+    './images/2.jpg',
+    './images/4.jpg',
+    './images/12.jpg',
+    './images/13.jpg',
+    './images/5.jpg',
+    './images/2.jpg'
 ], i=1;
 function csaHead(){
 
 if(i > (imgHead.length-1)){
-    $('.csa-head').animate({'opacity':'0'},600,function(){
+    $('.csa-head').animate({'opacity':'0'},700,function(){
         i=1;
-        $('.csa-head').css({'background':'url('+imgHead[0]+')'});
+        $('.csa-head')
+        .css({'background':'url('+imgHead[0]+')'})
+        .css({'background-repeat':'no-repeat'})
+        .css({'background-size':'cover'})
+        .css({'width':'100%'})
+        .css({'height':'100%'});
     });
-    $('.csa-head').animate({'opacity':'1'},600);
+    $('.csa-head').animate({'opacity':'1'},700);
 } else {
-    $('.csa-head').animate({'opacity':'0'},600,function(){
-        $('.csa-head').css({'background':'url('+imgHead[i]+')'});
+    $('.csa-head').animate({'opacity':'0'},700,function(){
+        $('.csa-head')
+        .css({'background':'url('+imgHead[i]+')'})
+        .css({'background-repeat':'no-repeat'})
+        .css({'background-size':'cover'})
+        .css({'width':'100%'})
+        .css({'height':'100%'})
+        ;
         i++;
     });
-    $('.csa-head').animate({'opacity':'1'},600);
+    $('.csa-head').animate({'opacity':'1'},700);
 }
 
 }
-var intervalCsaHead = setInterval(csaHead,4000);
+var intervalCsaHead = setInterval(csaHead,4500);
